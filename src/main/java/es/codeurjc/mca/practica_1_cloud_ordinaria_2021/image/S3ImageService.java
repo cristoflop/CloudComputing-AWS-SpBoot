68d0a36f56c7aeffe227c232751fc1f39fcac1f9
cristoflop <cristofer.lc.99@gmail.com>
2021-03-07 20:30:15 +0100

    S3 image service

---
+++ src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3ImageService.java
@@ -0,0 +1,58 @@
+package es.codeurjc.mca.practica_1_cloud_ordinaria_2021.image;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service("storageService")
+@Profile("production")
+public class S3ImageService implements ImageService {
+
+    @Value("${amazon.s3.bucket-name}")
+    private String BUCKET_NAME;
+
+    @Value("${amazon.s3.endpoint}")
+    private String ENDPOINT;
+
+    private final AmazonS3 s3 = AmazonS3ClientBuilder
+            .standard()
+            .withRegion(Regions.EU_WEST_1)
+            .build();
+
+    @Override
+    public String createImage(MultipartFile multiPartFile) {
+        createBucketIfNotExists();
+
+        String fileName = multiPartFile.getOriginalFilename();
+        File file = new File(System.getProperty("java.io.tmpdir")+"/"+fileName);
+        try {
+            multiPartFile.transferTo(file);
+        } catch (IOException e) {
+            throw new IllegalStateException("File upload failed");
+        }
+
+        PutObjectRequest por = new PutObjectRequest(BUCKET_NAME, fileName, file);
+        //if (isPublic) por.setCannedAcl(CannedAccessControlList.PublicRead);
+        s3.putObject(por);
+
+        return ENDPOINT + fileName;
+    }
+
+    private void createBucketIfNotExists() {
+        if(!s3.doesBucketExistV2(BUCKET_NAME)) s3.createBucket(BUCKET_NAME);
+    }
+
+    @Override
+    public void deleteImage(String image) {
+        s3.deleteObject(BUCKET_NAME, image);
+    }
+    
+}
